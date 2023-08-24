87c7d243210661e68f43eae28c670780ad7d1133
Zachary Shorts <shorts23@gmail.com>
2023-08-23 22:32:36 -0500

    Update 01-two-sum.js

--- 01-two-sum.js
+++ 01-two-sum.js
@@ -1,9 +1,8 @@
-/*
-  [] nums contains array of numbers
-  int target is the sum that we are looking for in nums
-*/
-function twoSum(nums, target) {
-
-}
-
-twoSum([2, 7, 11, 15], 9);
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    
+};
