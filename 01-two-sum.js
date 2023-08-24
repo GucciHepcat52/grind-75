112aded5dfba942052ebc78a02482a852371d35c
Dallin Breen <dallinbreen624@gmail.com>
2023-08-23 22:06:23 -0600

    Updated two sum file

--- 01-two-sum.js
+++ 01-two-sum.js
@@ -3,6 +3,42 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    
+var twoSum = function (nums, target) {
+  let result = [];
+  let hm = new Map();
+  // hashmap - lookup table - memoization(remember keydetails)
+  /*
+      {
+          7: 0(i),
+          -2:1,
+          -4: 2,
+
+      }
+  */
+  for (let i = 0; i < nums.length; i++) {
+    if (hm.has(nums[i])) result.push(i, hm.get(nums[i]));
+    hm.set(target - nums[i], i);
+  }
+
+  return result;
 };
+
+/*
+
+BRUTE FORCE
+
+  let result = [];
+
+  for (let i = 0; i < nums.length; i++) {
+      for (let j = i + 1; j < nums.length; j++) {
+          let addedNum = nums[i] + nums[j];
+          if (addedNum === target) {
+              result.push([nums[i], nums[j]])
+          }
+      }
+  }
+
+  console.log(result)
+  return result;
+  
+  */
