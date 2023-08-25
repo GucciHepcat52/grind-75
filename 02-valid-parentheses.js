ffd89e4c13aa95b0537869b4d1fe308f1cf86e66
Zachary Shorts <shorts23@gmail.com>
2023-08-25 12:42:33 -0500

    Update 02-valid-parentheses.js

--- 02-valid-parentheses.js
+++ 02-valid-parentheses.js
@@ -1,31 +1,35 @@
-function isValid(string) {
-  if (!string || string.length < 2) {
-    return false;
-  }
-
-  let stack = [];
-
-  for (let c of string) {
-    if (c === "(" || c === "{" || c === "[") {
-      stack.push(c);
-    } else {
-      if (!stack.length) {
-        return false;
-      }
-      let last = stack.pop();
-
-      if (c === ")" && last !== "(") {
-        return false;
-      }
-      if (c === "]" && last !== "[") {
-        return false;
-      }
-      if (c === "}" && last !== "{") {
-        return false;
-      }
-    }
-  }
-  return !stack.length;
-}
-
-isValid("()[]{}");
+/**
+    Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+    
+    An input string is valid if:
+    
+    Open brackets must be closed by the same type of brackets.
+    Open brackets must be closed in the correct order.
+    Every close bracket has a corresponding open bracket of the same type.
+     
+    
+    Example 1:
+    
+    Input: s = "()"
+    Output: true
+    Example 2:
+    
+    Input: s = "()[]{}"
+    Output: true
+    Example 3:
+    
+    Input: s = "(]"
+    Output: false
+     
+    
+    Constraints:
+    
+    1 <= s.length <= 104
+    s consists of parentheses only '()[]{}'.  
+    
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    
+};
