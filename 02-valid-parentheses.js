d14482c518dcc2fd6567cfaaae9e0681a8d52b06
Dallin Breen <dallinbreen624@gmail.com>
2023-08-25 13:39:23 -0600

    Updated files

--- 02-valid-parentheses.js
+++ 02-valid-parentheses.js
@@ -30,6 +30,50 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    
+
+var isValid = function (s) {
+  let stack = [];
+
+  for (let c of s) {
+    // Check to see if the array is empty and if the string is only one character
+    if (s.length < 2) return false;
+
+    // populate the stack
+    if (c === "(" || c === "{" || c === "[") {
+      stack.push(c);
+    } else {
+      if (stack.length === 0) return false;
+      // if a closing char is seen, pop off stack
+      let poppedValue = stack.pop();
+      if (poppedValue === "(" && c !== ")") return false;
+      if (poppedValue === "{" && c !== "}") return false;
+      if (poppedValue === "[" && c !== "]") return false;
+    }
+
+    // if pop off is not valid, finish
+
+    // if gets to end and nothing bad happened, true
+  }
+  return stack.length === 0;
 };
+
+/*
+
+Stack - data structure mimics a stack of papers on your desk
+    First values added into stack are the last values taken off
+    more recent added and more recent removed
+
+    operations on a stack
+        push = add on top of stack
+        pop = remove last item pushed
+
+
+    [1,2,3,4,5] -> 
+
+    5
+    4
+    3
+    2
+    1
+
+*/
--- 1207-unique-number-of-occurrences.js
+++ 1207-unique-number-of-occurrences.js
@@ -26,7 +26,44 @@
  * @return {boolean}
  */
 
+var uniqueOccurrences = function (arr) {
+  let hashmap = new Map();
+  let hashset = new Set();
 
-var uniqueOccurrences = function(arr) {
-    
+  // this loop saves all number of occurrences of each number in arr into hashmap
+  for (let num of arr) {
+    if (!hashmap.has(num)) {
+      hashmap.set(num, 1);
+    } else {
+      let count = hashmap.get(num);
+      hashmap.set(num, count + 1);
+    }
+  }
+
+  // this loop goes over all values in the hash ap and makes sure they only occur unique times by using a set, which only allows one instance of each element in the set
+  for (let key of hashmap) {
+    if (hashset.has(hashmap.get(key))) return false;
+    else {
+      hashset.add(hashmap.get(key));
+    }
+  }
+
+  /*
+          {
+              1: 3,
+              2: 2,
+              3: 1
+          }
+  
+          data structure called a set
+              set only allows a value to occur once
+  
+          let set = new Set();
+          set.add(2) // set: {2}
+          set.add(2) // set: {2}
+          set.add(3) // set: {2,3}
+  
+  
+          [1,2,2,1,1,3] turn this into a set === [1,2,3]
+      */
 };
